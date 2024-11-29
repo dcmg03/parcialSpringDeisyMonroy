87dbd66e608f8a1df767d13f488de64d16922b48
DeisyMonroy <deisy.monroy@uptc.edu.co>
2024-11-28 19:32:54 -0500

    customers

---
+++ src/main/java/edu/uptc/example/controller/customers.java
@@ -0,0 +1,56 @@
+package edu.uptc.example.controller;
+
+import edu.uptc.example.entityes.sale;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Customers")
+public class customers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(nullable = false, length = 20)
+    private String name;
+    @Column(nullable = false, length = 20)
+    private String lastName;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<sale> sales;
+
+    public customers() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(List<sale> sales) {
+        this.sales = sales;
+    }
+}
